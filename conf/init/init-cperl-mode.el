b8c40ce5624fc4b67bbf5831dd8823034e67ff0e
taqun <envoixnet@gmail.com>
2013-02-14 02:03:17 +0900

    apply cperl-mode to psgi file

--- conf/init/init-cperl-mode.el
+++ conf/init/init-cperl-mode.el
@@ -1,8 +1,9 @@
 (defalias 'perl-mode 'cperl-mode)
 
 (setq auto-mode-alist (append  auto-mode-alist))
-(add-to-list 'auto-mode-alist '("\\.cgi$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("\\.pm$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.cgi$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.psgi$" . cperl-mode))
 
 (setq cperl-indent-level 4)
 (setq cperl-continued-statement-offset 4)
@@ -12,7 +13,6 @@
 (setq cperl-close-paren-offset -4)
 (setq cperl-tab-always-indent t)
 (setq cperl-highlight-variables-indiscriminately t)
-
 
 (add-hook 'cperl-mode-hook
           (lambda()
