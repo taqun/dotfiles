11685b4457803b63d6769f4e401e177c6268af35
taqun <envoixnet@gmail.com>
2013-02-27 18:46:25 +0900

    use Project::libs for flymake in cperl-mode

--- conf/flymake.el
+++ conf/flymake.el
@@ -7,6 +7,14 @@
 (defadvice flymake-goto-prev-error (after flymake-goto-prev-error-display-message activate)
   (display-error-message))
 
+(defun flymake-perl-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "perl" (list "-MProject::Libs" "-wc" local-file))))
+
 (add-to-list 'flymake-allowed-file-name-masks '("\\.cgi$" flymake-perl-init))
 (add-to-list 'flymake-allowed-file-name-masks '("\\.psgi$" flymake-perl-init))
 
