dd26d12f97d9a07481222e2a8139c64954232c5a
taqun <envoixnet@gmail.com>
2013-05-24 18:07:17 +0900

    Add setting of tramp

--- init.el
+++ init.el
@@ -101,3 +101,10 @@
 ;; uniquify
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
+
+;; tramp
+(require 'tramp)
+(setq tramp-default-method "ssh")
+(add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist '("localhost" nil nil))
+(add-to-list 'tramp-default-proxies-alist '((regexp-quote (system-name)) nil nil))
