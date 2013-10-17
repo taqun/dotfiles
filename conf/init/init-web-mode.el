cfcf25be73ab97ebca4644fcd02aefa9b01396a4
taqun <envoixnet@gmail.com>
2013-10-17 18:37:32 +0900

    Update setting of web-mode

--- conf/init/init-web-mode.el
+++ conf/init/init-web-mode.el
@@ -2,6 +2,11 @@
 
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 
+(setq web-mode-tag-auto-close-style 2)
+
 (add-hook 'web-mode-hook 
           (lambda()
-            (auto-complete-mode t)))
+            (auto-complete-mode t)
+            (setq ac-sources '(ac-source-css-property
+                               ac-source-dictionary
+                               ac-source-filename))))
